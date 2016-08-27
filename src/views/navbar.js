1412d2a6e3ea93e15cc8eed083908e8612897329
Kevin Wang <wangkevin448@gmail.com>
2016-08-27 13:36:32 -0700

    transition navbar to react router links instead of a tags

--- src/views/navbar.js
+++ src/views/navbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router';
 
 import {hashCode} from 'utility';
 
@@ -15,18 +16,18 @@ class Navbar extends React.Component{
       <div class="nav-spacer"></div>
       <div class="nav-container">
         <div class="container">
-          <div class="brand brand-padded"><a href={this.props.brand[1]}><img src={this.props.brand[0]}/></a></div>
+          <div class="brand brand-padded"><Link to={this.props.brand[1]}><img src={this.props.brand[0]}/></Link></div>
           <ul class="nav-list">
             {this.props.list.map((i)=>{
               return <li key={hashCode(i[0]+i[1])}>
-                <a href={i[1]}>{i[0]}</a>
+                <Link to={i[1]}>{i[0]}</Link>
               </li>;
             })}
           </ul>
           <ul class="nav-list right">
             {this.props.listRight.map((i)=>{
               return <li key={hashCode(i[0]+i[1])}>
-                <a href={i[1]}>{i[0]}</a>
+                <Link to={i[1]}>{i[0]}</Link>
               </li>;
             })}
           </ul>
