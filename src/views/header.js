472f2df5ebc20acb8d8a9a93d5093532b0d5be47
Kevin Wang <wangkevin448@gmail.com>
2016-08-27 17:01:19 -0700

    header styles

--- src/views/header.js
+++ src/views/header.js
@@ -1,11 +1,14 @@
 import React from 'react';
-
+import 'styles/header.scss';
 
 class Header extends React.Component{
   render(){
     return <header>
-      <div className="container">
+      <div className="container header-child">
         {this.props.children}
+      </div>
+      <div className="downIcon">
+        <i className="fa fa-chevron-down" aria-hidden="true"></i>
       </div>
     </header>;
   }
