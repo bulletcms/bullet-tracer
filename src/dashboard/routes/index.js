25aa2147e61db029924b8104c1fbe9526d13bcd7
Kevin Wang <wangkevin448@gmail.com>
2016-08-28 23:00:54 -0700

    dashapp working

--- src/dashboard/routes/index.js
+++ src/dashboard/routes/index.js
@@ -7,6 +7,7 @@ import {Dne} from 'containers';
 
 const routes = <Router history={hashHistory}>
   <Route path="/" component={DashApp}>
+    <IndexRoute component={Dne}/>
     <Route path="/404" component={Dne}/>
     <Redirect from="*" to="/404" />
   </Route>
