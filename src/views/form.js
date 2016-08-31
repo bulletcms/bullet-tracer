e213298998931c68228c24b59a138e1e3b9837c8
Kevin Wang <wangkevin448@gmail.com>
2016-08-31 00:20:11 -0700

    updated form

--- src/views/form.js
+++ src/views/form.js
@@ -36,17 +36,23 @@ class Textarea extends React.Component{
    *   label       : string
    *   placeholder : string
    *   value       : string - initial value
+   *   cols        : columns for textbox
+   *   rows        : rows for textbox
    *   handleChange: function callback - passed input value
    */
   render(){
     return <div>
       {this.props.label && <span><label>{this.props.label}</label><br/></span>}
-      <textarea placeholder={this.props.placeholder || ""} value={this.state.value} onChange={(event)=>{
-        this.setState({value: event.target.value});
-        if(this.props.handleChange){
-          this.props.handleChange(event.target.value);
-        }
-      }}></textarea>
+      <textarea placeholder={this.props.placeholder || ""}
+        value={this.state.value}
+        cols={this.props.cols || 80}
+        rows={this.props.rows || 4}
+        onChange={(event)=>{
+          this.setState({value: event.target.value});
+          if(this.props.handleChange){
+            this.props.handleChange(event.target.value);
+          }
+        }}></textarea>
     </div>;
   }
 }
