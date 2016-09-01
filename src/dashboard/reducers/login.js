839239afe7ab4625d7d7f947045e05f9e5be8504
Kevin Wang <wangkevin448@gmail.com>
2016-08-31 22:54:17 -0700

    id token

--- src/dashboard/reducers/login.js
+++ src/dashboard/reducers/login.js
@@ -23,14 +23,26 @@ const LoginSaga = function*(){
   while(true){
     const loginaction = yield take(ACTIONS.login);
     yield cps([window.gapi, window.gapi.load], 'auth2');
-    console.log(window.gapi);
-    console.log(window.gapi.auth2);
     const auth2 = window.gapi.auth2.init({
       client_id: loginaction.clientId
     });
     const googleUser = yield call([auth2, auth2.signIn])
-    console.log(googleUser);
-    const logoutaction = yield take(ACTIONS.logout);
+    const signedIn = googleUser.isSignedIn();
+    if(signedIn){
+      const {id_token} = googleUser.getAuthResponse();
+      const profileObj = googleUser.getBasicProfile();
+      const profile = {
+        name: profileObj.getGivenName(),
+        fullName: profileObj.getName(),
+        lastName: profileObj.getFamilyName(),
+        email: profileObj.getEmail(),
+        googleId: profileObj.getId(),
+        profilePicture: profileObj.getImageUrl(),
+      };
+
+      const logoutaction = yield take(ACTIONS.logout);
+      yield call([auth2, auth2.signOut]);
+    }
   }
 };
 
