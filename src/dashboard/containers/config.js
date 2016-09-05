b038221752fc9f3bc8ca4ad9be431ba398a62636
Kevin Wang <wangkevin448@gmail.com>
2016-09-04 20:49:56 -0700

    config page

--- src/dashboard/containers/config.js
+++ src/dashboard/containers/config.js
@@ -3,14 +3,49 @@ import {connect} from 'react-redux';
 import Immutable from 'immutable';
 
 import {CONFIG} from 'dashboard/config';
+import {fetchConfigAction} from 'dashboard/reducers/actions';
+import {makeGetConfig, getConfigRequest, getLogin, getLoginExpiresAt, getLoginValid} from 'dashboard/reducers/selectors';
 import {Input, Textarea} from 'views';
 
 class Config extends React.Component {
+  componentWillMount(){
+    this.props.fetchConfig('navigation');
+  }
   render(){
     return <div>
-      Hello World!
+      {(!this.props.config.loading && !this.props.config.failed) && this.props.config.content &&
+        JSON.stringify(this.props.config.content)
+      }
     </div>;
   }
 }
+
+const makeMapStateToProps = ()=>{
+  const getConfig = makeGetConfig();
+  return (state)=>{
+    return {
+      config: getConfig(state),
+      request: getConfigRequest(state),
+      logininfo: getLogin(state),
+      loginExpiresAt: getLoginExpiresAt(state)
+    };
+  };
+};
+
+const mapDispatchToProps = (dispatch)=>{
+  return {
+    fetchConfig: (configid, method=false, body=false)=>{
+      dispatch(fetchConfigAction(CONFIG.retrieve('baseConfigUrl'), configid, method, body));
+    },
+    loginValid: (time)=>{
+      return getLoginValid(time);
+    }
+  };
+};
+
+Config = connect(
+  makeMapStateToProps,
+  mapDispatchToProps
+)(Config);
 
 export {Config};
