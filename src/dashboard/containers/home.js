afef817ba187b0381801b3f0e1be6424fb82590e
Kevin Wang <wangkevin448@gmail.com>
2016-09-06 23:42:48 -0700

    new user login

--- src/dashboard/containers/home.js
+++ src/dashboard/containers/home.js
@@ -2,7 +2,8 @@ import React from 'react';
 import {connect} from 'react-redux';
 
 import {CONFIG} from 'dashboard/config';
-import {signInWithGoogleAction} from 'dashboard/reducers/actions';
+import {newUserAction, signInWithGoogleAction} from 'dashboard/reducers/actions';
+import {getNewUserRequest} from 'dashboard/reducers/selectors';
 import {Input, Modal} from 'views';
 
 
@@ -61,6 +62,13 @@ class Home extends React.Component{
           }}>Setup</button>
       </div>
 
+      {this.props.newUserRequest &&
+        <pre className="request-status">
+          {this.props.newUserRequest.status && <span>new user request success for user: {this.props.newUserRequest.username}</span>}
+          {!this.props.newUserRequest.status && <span>new user request failed for user: {this.props.newUserRequest.username}</span>}
+        </pre>
+      }
+
       <Modal isOpen={this.state.createUser}>
         <h1>Create Account</h1>
         <div className="button-row">
@@ -68,23 +76,31 @@ class Home extends React.Component{
         </div>
         <div className="button-row">
           <button className="button-outline-primary" onClick={()=>{
-              this.props.signInWithGoogle(async (data)=>{this.setState({...this.state, newUser: {...this.state.newUser, ...data}});});
-            }}>Sign In with Google</button>
-          </div>
-          <Input label="username" value={this.state.newUser.username} error={this.state.userErrors.username}
-            handleBlur={(value)=>{this.setState({...this.state, newUser: {...this.state.newUser, username: value}});}}/>
-          <Input label="name" value={this.state.newUser.name} error={this.state.userErrors.name}
-            handleBlur={(value)=>{this.setState({...this.state, newUser: {...this.state.newUser, name: value}});}}/>
-          <Input label="last name" value={this.state.newUser.lastName} error={this.state.userErrors.lastName}
-            handleBlur={(value)=>{this.setState({...this.state, newUser: {...this.state.newUser, lastName: value}});}}/>
-          <Input label="full name" value={this.state.newUser.fullName} error={this.state.userErrors.fullName}
-            handleBlur={(value)=>{this.setState({...this.state, newUser: {...this.state.newUser, fullName: value}});}}/>
-          <Input label="email" value={this.state.newUser.email} error={this.state.userErrors.email}
-            handleBlur={(value)=>{this.setState({...this.state, newUser: {...this.state.newUser, email: value}});}}/>
-          <Input label="Google Id" value={this.state.newUser.googleId} error={this.state.userErrors.googleId}
-            handleBlur={(value)=>{this.setState({...this.state, newUser: {...this.state.newUser, googleId: value}});}}/>
-          <Input label="profile picture" value={this.state.newUser.profilePicture} error={this.state.userErrors.profilePicture}
-            handleBlur={(value)=>{this.setState({...this.state, newUser: {...this.state.newUser, profilePicture: value}});}}/>
+            this.props.signInWithGoogle(async (data)=>{this.setState({...this.state, newUser: {...this.state.newUser, ...data}});});
+          }}>Sign In with Google</button>
+        </div>
+
+        <Input label="username" value={this.state.newUser.username} error={this.state.userErrors.username}
+          handleBlur={(value)=>{this.setState({...this.state, newUser: {...this.state.newUser, username: value}});}}/>
+        <Input label="name" value={this.state.newUser.name} error={this.state.userErrors.name}
+          handleBlur={(value)=>{this.setState({...this.state, newUser: {...this.state.newUser, name: value}});}}/>
+        <Input label="last name" value={this.state.newUser.lastName} error={this.state.userErrors.lastName}
+          handleBlur={(value)=>{this.setState({...this.state, newUser: {...this.state.newUser, lastName: value}});}}/>
+        <Input label="full name" value={this.state.newUser.fullName} error={this.state.userErrors.fullName}
+          handleBlur={(value)=>{this.setState({...this.state, newUser: {...this.state.newUser, fullName: value}});}}/>
+        <Input label="email" value={this.state.newUser.email} error={this.state.userErrors.email}
+          handleBlur={(value)=>{this.setState({...this.state, newUser: {...this.state.newUser, email: value}});}}/>
+        <Input label="Google Id" value={this.state.newUser.googleId} error={this.state.userErrors.googleId}
+          handleBlur={(value)=>{this.setState({...this.state, newUser: {...this.state.newUser, googleId: value}});}}/>
+        <Input label="profile picture" value={this.state.newUser.profilePicture} error={this.state.userErrors.profilePicture}
+          handleBlur={(value)=>{this.setState({...this.state, newUser: {...this.state.newUser, profilePicture: value}});}}/>
+
+        <div className="button-row">
+          <button className="button-primary" onClick={()=>{
+            this.props.newUser({data: this.state.newUser});
+            this.setState({...this.state, createUser: false, newUser: false, userErrors: false});
+          }}>Create Account</button>
+        </div>
       </Modal>
 
       <Modal isOpen={this.state.beginSetup}>
@@ -99,6 +115,7 @@ class Home extends React.Component{
 
 const mapStateToProps = (state)=>{
   return {
+    newUserRequest: getNewUserRequest(state)
   };
 };
 
@@ -106,6 +123,9 @@ const mapDispatchToProps = (dispatch, props)=>{
   return {
     signInWithGoogle: (callback)=>{
       dispatch(signInWithGoogleAction(CONFIG.retrieve('loginClientId'), callback));
+    },
+    newUser: (body)=>{
+      dispatch(newUserAction(CONFIG.retrieve('baseUsersUrl'), body));
     }
   };
 };
