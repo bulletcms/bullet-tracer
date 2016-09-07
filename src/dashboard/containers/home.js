190f20a6d19bcc290a7b858e4b0601b6bdf2e987
Kevin Wang <wangkevin448@gmail.com>
2016-09-06 23:05:53 -0700

    component will receive props on input component

--- src/dashboard/containers/home.js
+++ src/dashboard/containers/home.js
@@ -22,8 +22,8 @@ const blankErrors = ()=>{
   return {
     username: false,
     name: false,
-    fullName: false,
     lastName: false,
+    fullName: false,
     email: false,
     googleId: false,
     profilePicture: false,
@@ -70,24 +70,21 @@ class Home extends React.Component{
           <button className="button-outline-primary" onClick={()=>{
               this.props.signInWithGoogle(async (data)=>{this.setState({...this.state, newUser: {...this.state.newUser, ...data}});});
             }}>Sign In with Google</button>
-          <button className="button-outline-primary" onClick={()=>{
-              console.log(this.state.newUser);
-            }}>Print</button>
-        </div>
-        <Input label="username" value={this.state.newUser.username} error={this.state.userErrors.username}
-          handleBlur={(value)=>{this.setState({...this.state, newUser: {...this.state.newUser, username: value}});}}/>
-        <Input label="name" value={this.state.newUser.name} error={this.state.userErrors.name}
-          handleBlur={(value)=>{this.setState({...this.state, newUser: {...this.state.newUser, name: value}});}}/>
-        <Input label="last name" value={this.state.newUser.lastName} error={this.state.userErrors.lastName}
-          handleBlur={(value)=>{this.setState({...this.state, newUser: {...this.state.newUser, lastName: value}});}}/>
-        <Input label="full name" value={this.state.newUser.fullName} error={this.state.userErrors.fullName}
-          handleBlur={(value)=>{this.setState({...this.state, newUser: {...this.state.newUser, fullName: value}});}}/>
-        <Input label="email" value={this.state.newUser.email} error={this.state.userErrors.email}
-          handleBlur={(value)=>{this.setState({...this.state, newUser: {...this.state.newUser, email: value}});}}/>
-        <Input label="Google Id" value={this.state.newUser.googleId} error={this.state.userErrors.googleId}
-          handleBlur={(value)=>{this.setState({...this.state, newUser: {...this.state.newUser, googleId: value}});}}/>
-        <Input label="profile picture" value={this.state.newUser.profilePicture} error={this.state.userErrors.profilePicture}
-          handleBlur={(value)=>{this.setState({...this.state, newUser: {...this.state.newUser, profilePicture: value}});}}/>
+          </div>
+          <Input label="username" value={this.state.newUser.username} error={this.state.userErrors.username}
+            handleBlur={(value)=>{this.setState({...this.state, newUser: {...this.state.newUser, username: value}});}}/>
+          <Input label="name" value={this.state.newUser.name} error={this.state.userErrors.name}
+            handleBlur={(value)=>{this.setState({...this.state, newUser: {...this.state.newUser, name: value}});}}/>
+          <Input label="last name" value={this.state.newUser.lastName} error={this.state.userErrors.lastName}
+            handleBlur={(value)=>{this.setState({...this.state, newUser: {...this.state.newUser, lastName: value}});}}/>
+          <Input label="full name" value={this.state.newUser.fullName} error={this.state.userErrors.fullName}
+            handleBlur={(value)=>{this.setState({...this.state, newUser: {...this.state.newUser, fullName: value}});}}/>
+          <Input label="email" value={this.state.newUser.email} error={this.state.userErrors.email}
+            handleBlur={(value)=>{this.setState({...this.state, newUser: {...this.state.newUser, email: value}});}}/>
+          <Input label="Google Id" value={this.state.newUser.googleId} error={this.state.userErrors.googleId}
+            handleBlur={(value)=>{this.setState({...this.state, newUser: {...this.state.newUser, googleId: value}});}}/>
+          <Input label="profile picture" value={this.state.newUser.profilePicture} error={this.state.userErrors.profilePicture}
+            handleBlur={(value)=>{this.setState({...this.state, newUser: {...this.state.newUser, profilePicture: value}});}}/>
       </Modal>
 
       <Modal isOpen={this.state.beginSetup}>
--- src/views/form.js
+++ src/views/form.js
@@ -6,6 +6,10 @@ class Input extends React.Component{
     this.state = {value: props.value || ''};
   }
 
+  componentWillReceiveProps(nextProps) {
+    this.setState({value: nextProps.value || ''});
+  }
+
   /**
    * props:
    *   label       : string
