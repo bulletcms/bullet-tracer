41d44e55e238f949043c5b83ddc3f7738a5cc18f
Kevin Wang <wangkevin448@gmail.com>
2016-09-07 23:34:32 -0700

    setup process

--- src/dashboard/containers/home.js
+++ src/dashboard/containers/home.js
@@ -58,7 +58,7 @@ class Home extends React.Component{
       <div className="button-row">
         <button className="button-outline"
           onClick={()=>{
-            this.setState({...this.state, beginSetup: true});
+            this.setState({...this.state, beginSetup: true, newUser: blankUser(), userErrors: blankErrors()});
           }}>Setup</button>
       </div>
 
@@ -68,11 +68,17 @@ class Home extends React.Component{
           {!this.props.newUserRequest.status && <span>new user request failed for user: {this.props.newUserRequest.username}</span>}
         </pre>
       }
+      {this.props.setupRequest &&
+        <pre className="request-status">
+          {this.props.setupRequest.status && <span>setup request success</span>}
+          {!this.props.setupRequest.status && <span>setup request failed</span>}
+        </pre>
+      }
 
       <Modal isOpen={this.state.createUser}>
         <h1>Create Account</h1>
         <div className="button-row">
-          <button onClick={()=>{this.setState({...this.state, createUser: false, newUser: false});}}>Cancel</button>
+          <button onClick={()=>{this.setState({...this.state, createUser: false, newUser: false, userErrors: false});}}>Cancel</button>
         </div>
         <div className="button-row">
           <button className="button-outline-primary" onClick={()=>{
@@ -106,7 +112,34 @@ class Home extends React.Component{
       <Modal isOpen={this.state.beginSetup}>
         <h1>Setup BulletCMS</h1>
         <div className="button-row">
-          <button onClick={()=>{this.setState({...this.state, beginSetup: false});}}>Cancel</button>
+          <button onClick={()=>{this.setState({...this.state, beginSetup: false, newUser: false, userErrors: false});}}>Cancel</button>
+        </div>
+        <div className="button-row">
+          <button className="button-outline-primary" onClick={()=>{
+            this.props.signInWithGoogle(async (data)=>{this.setState({...this.state, newUser: {...this.state.newUser, ...data}});});
+          }}>Sign In with Google</button>
+        </div>
+
+        <Input label="username" value={this.state.newUser.username} error={this.state.userErrors.username}
+          handleBlur={(value)=>{this.setState({...this.state, newUser: {...this.state.newUser, username: value}});}}/>
+        <Input label="name" value={this.state.newUser.name} error={this.state.userErrors.name}
+          handleBlur={(value)=>{this.setState({...this.state, newUser: {...this.state.newUser, name: value}});}}/>
+        <Input label="last name" value={this.state.newUser.lastName} error={this.state.userErrors.lastName}
+          handleBlur={(value)=>{this.setState({...this.state, newUser: {...this.state.newUser, lastName: value}});}}/>
+        <Input label="full name" value={this.state.newUser.fullName} error={this.state.userErrors.fullName}
+          handleBlur={(value)=>{this.setState({...this.state, newUser: {...this.state.newUser, fullName: value}});}}/>
+        <Input label="email" value={this.state.newUser.email} error={this.state.userErrors.email}
+          handleBlur={(value)=>{this.setState({...this.state, newUser: {...this.state.newUser, email: value}});}}/>
+        <Input label="Google Id" value={this.state.newUser.googleId} error={this.state.userErrors.googleId}
+          handleBlur={(value)=>{this.setState({...this.state, newUser: {...this.state.newUser, googleId: value}});}}/>
+        <Input label="profile picture" value={this.state.newUser.profilePicture} error={this.state.userErrors.profilePicture}
+          handleBlur={(value)=>{this.setState({...this.state, newUser: {...this.state.newUser, profilePicture: value}});}}/>
+
+        <div className="button-row">
+          <button className="button-primary" onClick={()=>{
+            this.props.setup({data: this.state.newUser});
+            this.setState({...this.state, beginSetup: false, newUser: false, userErrors: false});
+          }}>Create Account</button>
         </div>
       </Modal>
     </div>;
